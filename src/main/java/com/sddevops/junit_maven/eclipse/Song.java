1d7de42bb5a58d81bf357c20e06077c76d1033fd
Ershath47 <mohamedershath@ymail.com>
2026-08-29 18:41:27 +0800

    Add Song class with properties and methods

--- src/main/java/com/sddevops/junit_maven/eclipse/Song.java
+++ src/main/java/com/sddevops/junit_maven/eclipse/Song.java
@@ -1,1 +1,26 @@
+package com.sddevops.junit_maven.eclipse;
+
+public class Song {
+    private String id;
+    private String title;
+    private String artist;
+    private double songLength;
+
+    public Song(String id, String title, String artist, double songLength) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.songLength = songLength;
+    }
+
+    public String getId() { return id; }
+    public String getTitle() { return title; }
+    public String getArtist() { return artist; }
+    public double getSongLength() { return songLength; }
+
+    @Override
+    public String toString() {
+        return title + " by " + artist;
+    }
+}
 
